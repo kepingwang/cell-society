7140865a495dbf217c719d5776eb24513ace6d18
Keping <kw238@duke.edu>
2017-01-29 21:51:35 -0500

    Switching between scenes

--- src/cellsociety/SettingsScreen.java
+++ src/cellsociety/SettingsScreen.java
@@ -1,5 +1,10 @@
 package cellsociety;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
 /**
  * The screen where users can make some settings.
  * @author keping
@@ -7,4 +12,58 @@ package cellsociety;
  */
 public class SettingsScreen {
 
+	private SceneController controller;
+	private Scene scene;
+	
+	// configuration variables
+	// To be displayed to the user
+	private String societyID;
+	private int rows;
+	private int cols;
+	private double width;
+	private double height;
+	private double stepDelay;
+	
+	public String getSocietyID() { return societyID; }
+	public int getRows() { return rows; }
+	public int getCols() { return cols; }
+	public double getWidth() { return width; }
+	public double getHeight() { return height; }
+	public double getStepDelay() { return stepDelay; }
+	
+
+	// Buttons and texts for display
+	Button buttonMain;
+	Button buttonPlay;
+	
+	public SettingsScreen(SceneController controller, String societyID) {
+		this.controller = controller;
+		this.societyID = societyID;
+		initScene();
+	}
+	
+	private void initScene() {
+		// TODO: add buttons and text fields for settings
+		Group root = new Group();
+		scene = new Scene(root, SceneController.WIDTH, SceneController.HEIGHT);
+		VBox vBox = new VBox();
+		root.getChildren().add(vBox);
+		buttonMain = new Button("Back");
+		buttonPlay = new Button("Play");
+		vBox.getChildren().addAll(buttonMain, buttonPlay);
+		initEventHandlers();
+	}
+
+	private void initEventHandlers() {
+		buttonMain.setOnMouseClicked(e -> controller.backToMain());
+		buttonPlay.setOnMouseClicked(e -> {
+			Society society = new Society(controller, this);
+			society.show();
+		});
+	}
+
+	public void show() {
+		controller.setScene(scene);
+	}
+	
 }
--- src/cellsociety/Society.java
+++ src/cellsociety/Society.java
@@ -2,18 +2,48 @@ package cellsociety;
 
 import java.util.List;
 
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 
 public class Society {
+	private SceneController controller;
+	private SettingsScreen settings;
 	private Scene scene;
+	
+	private Button buttonMain;
+	private Button buttonSettings;
+	private Button buttonPlay;
+	
 	
 	private int rows;
 	private int cols;
 	private Cell[][] cells;
 	
+	
+	public Society(SceneController controller, SettingsScreen settings) {
+		this.controller = controller;
+		this.settings = settings;
+		Group root = new Group();
+		scene = new Scene(root, SceneController.WIDTH, SceneController.HEIGHT);
+		HBox hBox = new HBox();
+		root.getChildren().add(hBox);
+		buttonMain = new Button("Back to Main");
+		buttonSettings = new Button("Back to Settings");
+		buttonPlay = new Button("PLAY");
+		hBox.getChildren().addAll(buttonMain, buttonSettings, buttonPlay);
+		initEventHandlers();
+	}
+ 	
+	private void initEventHandlers() {
+		buttonMain.setOnMouseClicked(e -> controller.backToMain());
+		buttonSettings.setOnMouseClicked(e -> settings.show());
+		buttonPlay.setOnMouseClicked(e -> play());
+	}
+	
 	// TODO: constructor
-	public Society(Config config) {
+	public Society(SettingsScreen settings) {
 		// TODO: init cells
 		cells = new Cell[rows][cols];
 		for (int i = 0; i < rows; i++) {
@@ -49,11 +79,17 @@ public class Society {
 		syncCells();
 	}
 
+	public void play() {
+		// TODO
+	}
+	
 	public Scene getScene() {
 		return scene;
 	}
 	
-	
-	Button button;
+	public void show() {
+		controller.setScene(scene);
+	}
+
 	
 }
