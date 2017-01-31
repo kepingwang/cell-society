86c0ba288ee5eb88f7b10402934c38609296f1a8
Gordon Huynh <ghh6@duke.edu>
2017-01-30 22:43:38 -0500

    added buttons and methods to control simulation viewing

--- src/cellsociety/Society.java
+++ src/cellsociety/Society.java
@@ -13,22 +13,33 @@ import javafx.util.Duration;
 
 
 public class Society {
-	private final int FRAMES_PER_SECOND = 60;
+	private final int FRAMES_PER_SECOND = 2;
 	private final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
 	private SceneController controller;
 	private SettingsScreen settings;
 	private Scene scene;
 	private Group root;
 	private Timeline timeline;
+	private KeyFrame frame;
 	private Rule rule;
 
+	private double speedMultiplier;
 	private Button buttonMain;
 	private Button buttonSettings;
 	private Button buttonPlay;
+	private Button buttonPause;
+	private Button buttonResume;
+	private Button buttonFastForward;
+	private Button buttonSlowForward;
+	private Button buttonAdvanceFrame;
 
 	private Cell[][] cells;
 	private int[][] states;
 
+	public Scene getScene() {
+		return scene;
+	}
+	
 	public Society(SceneController controller, SettingsScreen settings, Cell[][] cells) {
 		this.controller = controller;
 		this.settings = settings;
@@ -42,8 +53,174 @@ public class Society {
 		buttonMain.setOnMouseClicked(e -> controller.backToMain());
 		buttonSettings.setOnMouseClicked(e -> settings.show());
 		buttonPlay.setOnMouseClicked(e -> startSimulation());
+		buttonPause.setOnMouseClicked(e -> this.pauseSimulation());
+		buttonResume.setOnMouseClicked(e -> this.resumeSimulation());
+		buttonFastForward.setOnMouseClicked(e -> this.fastForward());
+		buttonSlowForward.setOnMouseClicked(e -> this.slowForward());
+		buttonAdvanceFrame.setOnMouseClicked(e -> this.advanceFrame());
 	}
 
+	private void updateCells() {
+		for (int i = 0; i < cells.length; i++) {
+			for (int j = 0; j < cells[0].length; j++) {
+				Cell[] neighbors = getNeighbors(i, j);
+				cells[i][j].update(neighbors);
+			}
+		}
+	}
+
+	private void syncCells() {
+		for (int i = 0; i < cells.length; i++) {
+			for (int j = 0; j < cells[0].length; j++) {
+				cells[i][j].syncState();
+			}
+		}
+	}
+
+	/**
+	 * Update the cell states in one round.
+	 */
+	public void update() {
+		updateCells(); // TODO
+		syncCells();
+	}
+	
+	/**
+	 * Create the scene for simulator
+	 */
+	// TODO: scene setup
+	private void setUpScene() {
+		Group root = new Group();
+		scene = new Scene(root, SceneController.WIDTH, SceneController.HEIGHT);
+		VBox vBox = new VBox();
+		root.getChildren().add(vBox);
+		buttonMain = new Button("Back to Main");
+		buttonSettings = new Button("Back to Settings");
+		buttonPlay = new Button("PLAY");
+		buttonPause = new Button("Pause");
+		buttonResume = new Button("Resume");
+		buttonFastForward = new Button("Speed Up");
+		buttonSlowForward = new Button("Slow Down");
+		buttonAdvanceFrame = new Button("Forward Frame");
+		vBox.getChildren().addAll(buttonMain, buttonSettings, buttonPlay, buttonPause, buttonResume, buttonFastForward, buttonSlowForward, buttonAdvanceFrame);
+		Group cellGroup = new Group();
+		for (Cell[] cellRow : cells) {
+			for (Cell cell : cellRow) {
+				cellGroup.getChildren().add(cell);
+			}
+		}
+		vBox.getChildren().add(cellGroup);
+	}
+
+	/**
+	 * Create the HUD for the simulator
+	 */
+	// TODO: HUD setup
+	private void setUpHUD() {
+
+	}
+
+	/**
+	 * Generate list of neighbors surrounding target cell List indices correlate
+	 * to neighbors as follows:
+	 * 0 1 2
+	 * 3 X 4
+	 * 5 6 7
+	 */
+	private Cell[] getNeighbors(int x, int y) {
+		int rows = cells.length;
+		int cols = cells[0].length;
+		Cell[] retArray = new Cell[8];
+		if (x > 0) {
+			retArray[1] = cells[x - 1][y];
+			if (y > 0) {
+				retArray[0] = cells[x - 1][y - 1];
+			}
+			if (y < cols - 1) {
+				retArray[2] = cells[x - 1][y + 1];
+			}
+		}
+		if (x < rows - 1) {
+			retArray[6] = cells[x + 1][y];
+			if (y > 0) {
+				retArray[5] = cells[x + 1][y - 1];
+			}
+			if (y < cols - 1) {
+				retArray[7] = cells[x + 1][y + 1];
+			}
+		}
+		if (y > 0) {
+			retArray[3] = cells[x][y - 1];
+		}
+		if (y < cols - 1) {
+			retArray[4] = cells[x][y + 1];
+		}
+		return retArray;
+	}
+
+	public void show() {
+		controller.setScene(scene);
+	}
+	
+	/**
+	 * Method to begin simulation Must be called in order to start a timeline
+	 */
+	public void startSimulation() {
+		speedMultiplier = 1;
+		KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> update());
+		timeline = new Timeline();
+		timeline.setCycleCount(Animation.INDEFINITE);
+		timeline.getKeyFrames().add(frame);
+		timeline.play();
+	}
+	
+	/**
+	 * Method to pause simulation
+	 */
+	private void pauseSimulation(){
+		timeline.pause();
+	}
+	
+	/**
+	 * Method to resume simulation
+	 */
+	private void resumeSimulation(){
+		timeline.play();
+	}
+	
+	/**
+	 * Method to fast forward simulation
+	 */
+	private void fastForward(){
+		timeline.stop();
+		if(speedMultiplier >= .25){
+			speedMultiplier -= .25;
+		}
+		timeline.getKeyFrames().clear();
+		frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY * speedMultiplier), e -> update());
+		timeline.getKeyFrames().add(frame);
+		timeline.play();
+	}
+	
+	/**
+	 * Method to slow down simulation
+	 */
+	private void slowForward(){
+		timeline.stop();
+		speedMultiplier += .25;
+		timeline.getKeyFrames().clear();
+		frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY * speedMultiplier), e -> update());
+		timeline.getKeyFrames().add(frame);
+		timeline.play();
+	}
+	
+	/**
+	 * Method to step forward a frame, will automatically pause timeline if not pause already
+	 */
+	private void advanceFrame(){
+		timeline.pause();
+		this.update();
+	}
 	
 	/**
 	 * For testing
@@ -147,119 +324,5 @@ public class Society {
 		else{
 			states[x][y] = 0;
 		}
-	}
-
-	/**
-	 * Method to begin simulation Must be called in order to start a timeline
-	 */
-	public void startSimulation() {
-		KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> update());
-		timeline = new Timeline();
-		timeline.setCycleCount(Animation.INDEFINITE);
-		timeline.getKeyFrames().add(frame);
-		timeline.play();
-	}
-
-	private void updateCells() {
-		for (int i = 0; i < cells.length; i++) {
-			for (int j = 0; j < cells[0].length; j++) {
-				Cell[] neighbors = getNeighbors(i, j);
-				cells[i][j].update(neighbors);
-			}
-		}
-	}
-
-	private void syncCells() {
-		for (int i = 0; i < cells.length; i++) {
-			for (int j = 0; j < cells[0].length; j++) {
-				cells[i][j].syncState();
-			}
-		}
-	}
-
-	/**
-	 * Update the cell states in one round.
-	 */
-	public void update() {
-		updateCells(); // TODO
-		syncCells();
-	}
-
-	public Scene getScene() {
-		return scene;
-	}
-
-	
-	
-	/**
-	 * Create the scene for simulator
-	 */
-	// TODO: scene setup
-	private void setUpScene() {
-		Group root = new Group();
-		scene = new Scene(root, SceneController.WIDTH, SceneController.HEIGHT);
-		VBox vBox = new VBox();
-		root.getChildren().add(vBox);
-		buttonMain = new Button("Back to Main");
-		buttonSettings = new Button("Back to Settings");
-		buttonPlay = new Button("PLAY");
-		vBox.getChildren().addAll(buttonMain, buttonSettings, buttonPlay);
-		Group cellGroup = new Group();
-		for (Cell[] cellRow : cells) {
-			for (Cell cell : cellRow) {
-				cellGroup.getChildren().add(cell);
-			}
-		}
-		vBox.getChildren().add(cellGroup);
-	}
-
-	/**
-	 * Create the HUD for the simulator
-	 */
-	// TODO: HUD setup
-	private void setUpHUD() {
-
-	}
-
-	/**
-	 * Generate list of neighbors surrounding target cell List indices correlate
-	 * to neighbors as follows:
-	 * 0 1 2
-	 * 3 X 4
-	 * 5 6 7
-	 */
-	private Cell[] getNeighbors(int x, int y) {
-		int rows = cells.length;
-		int cols = cells[0].length;
-		Cell[] retArray = new Cell[8];
-		if (x > 0) {
-			retArray[1] = cells[x - 1][y];
-			if (y > 0) {
-				retArray[0] = cells[x - 1][y - 1];
-			}
-			if (y < cols - 1) {
-				retArray[2] = cells[x - 1][y + 1];
-			}
-		}
-		if (x < rows - 1) {
-			retArray[6] = cells[x + 1][y];
-			if (y > 0) {
-				retArray[5] = cells[x + 1][y - 1];
-			}
-			if (y < cols - 1) {
-				retArray[7] = cells[x + 1][y + 1];
-			}
-		}
-		if (y > 0) {
-			retArray[3] = cells[x][y - 1];
-		}
-		if (y < cols - 1) {
-			retArray[4] = cells[x][y + 1];
-		}
-		return retArray;
-	}
-
-	public void show() {
-		controller.setScene(scene);
 	}
 }
