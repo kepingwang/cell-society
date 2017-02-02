610159529932a9cc4732755af2e5e7988366db9c
Gordon Huynh <ghh6@duke.edu>
2017-02-01 20:43:34 -0500

    Merge rules from my branch

--- src/core/rules/GameOfLifeRule.java
+++ src/core/rules/GameOfLifeRule.java
@@ -1,15 +1,37 @@
 package core.rules;
 
 import core.Cell;
+import javafx.scene.paint.Color;
 
 /**
- * Rules for cells in Game Of Life
+ * Rules for Game of Life simulation
  * @author Gordon
  *
  */
-
 public class GameOfLifeRule implements Rule {
+	private final int UNDER_POP_LIMIT = 2;
+	private final int OVER_POP_LIMIT = 3;
+	private final int SPAWN_REQUIREMENT = 3;
+	
+	private final int DEAD = 0;
+	private final int ALIVE = 1;
+	
+	private Color[] colors;
+	
+	public GameOfLifeRule(Color[] colorIn){
+		colors = colorIn;
+	}
+	
+	public GameOfLifeRule(){
+		this(new Color[] {Color.WHITE, Color.BLACK});
+	}
 
+	/**
+	 * Cells that are alive will die if there are less than
+	 * 2 or more than 3 living cells around it
+	 * (overpopulation/underpopulation)
+	 * Cells can be born if exactly 3 cells are nearby
+	 */
 	@Override
 	public int update(Cell cell, Cell[] neighbors) {
 		int cellState = cell.getState();
@@ -19,13 +41,22 @@ public class GameOfLifeRule implements Rule {
 				numLiving += neighbors[i].getState(); 
 			}
 		}
-		if(cellState == 1 && (numLiving < 2 || numLiving > 3)){
-			cellState = 0;
+		if(cellState == ALIVE && (numLiving < UNDER_POP_LIMIT || numLiving > OVER_POP_LIMIT)){
+			cellState = DEAD;
 		}
-		else if(cellState == 0 && numLiving == 3){
-			cellState = 1;
+		else if(cellState == DEAD && numLiving == SPAWN_REQUIREMENT){
+			cellState = ALIVE;
 		}
 		return cellState;
+	}
+
+	public Color[] getColor() {
+		// TODO Auto-generated method stub
+		return colors;
+	}
+
+	public Color updateColor(Cell cell) {
+		return colors[cell.getState()];
 	}
 
 }
