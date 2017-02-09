b95336179112af3194bbecef9ceacbad6cc23ffb
Gordon Huynh <ghh6@duke.edu>
2017-02-09 14:52:11 -0500

    Unfinished implementation of foraging ants

--- src/core/Ant.java
+++ src/core/Ant.java
@@ -1,5 +1,81 @@
 package core;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+import java.util.TreeSet;
+
 public class Ant {
-	private 
+	private static final int NUM_OF_NEIGHBORS = 8;
+	private ForagingAntCell currentCell;
+	private boolean hasFood;
+	private int direction;
+	private Random antRNG = new Random();
+	
+	/**
+	 * Constructor for ant
+	 * Requires the cell it is born in
+	 * Generates a random direction based off it's neighbors
+	 * @param cellIn
+	 */
+	public Ant(ForagingAntCell cellIn){
+		currentCell = cellIn;
+		direction = antRNG.nextInt(NUM_OF_NEIGHBORS);
+	}
+	
+	/**
+	 * checks surroundings for next cell to move towards
+	 * returns 1 if ant successfully moves
+	 * returns 0 if ant remains in same cell
+	 * @param neighbors
+	 * @return
+	 */
+	public int forage(List<Cell> neighborsIn){
+		ArrayList<ForagingAntCell> neighbors = new ArrayList<ForagingAntCell>();
+		for(Cell c : neighborsIn){
+			neighbors.add((ForagingAntCell) c);
+		}
+		if(this.hasFood){
+			return this.returnHome(neighbors);
+		}
+		return this.findFood(neighbors);
+	}
+	
+	private int returnHome(ArrayList<ForagingAntCell> neighbors){
+		if(currentCell.isFood()){
+			TreeSet<ForagingAntCell> tempTree = new TreeSet<ForagingAntCell>(new HomeScentComp());
+			direction = 
+		}
+		return 1;
+	}
+	
+	private int findFood(ArrayList<ForagingAntCell> neighbors){
+		
+		return 1;
+	}
+}
+
+class FoodScentComp implements Comparator<ForagingAntCell>{
+
+	@Override
+	public int compare(ForagingAntCell cell1, ForagingAntCell cell2) {
+		if(cell1.getFoodScent() > cell2.getFoodScent()){
+			return 1;
+		}
+		return -1;
+	}
+}
+
+class HomeScentComp implements Comparator<ForagingAntCell>{
+
+	@Override
+	public int compare(ForagingAntCell cell1, ForagingAntCell cell2) {
+		// TODO Auto-generated method stub
+		if(cell1.getHomeScent() > cell2.getHomeScent()){
+			return 1;
+		}
+		return -1;
+	}
+	
 }
--- src/core/ForagingAntCell.java
+++ src/core/ForagingAntCell.java
@@ -2,12 +2,73 @@ package core;
 
 import java.util.List;
 
+/**
+ * Class to represent a cell in Foraging Ant Simulation
+ * @author Gordon
+ *
+ */
 public class ForagingAntCell extends Cell{
+	public static final int NEUTRAL_CELL = 0;
+	public static final int NEST_CELL = 1;
+	public static final int FOOD_CELL = 2;
+	
 	private List<Ant> occupyingAnts;
+	private boolean nestCell;
+	private boolean foodCell;
+	private double foodScent;
+	private double homeScent;
+	private int foodAmount;
 
+	/**
+	 * General constructor similar to cell constructor
+	 * @param x
+	 * @param y
+	 * @param w
+	 * @param h
+	 * @param state
+	 */
 	public ForagingAntCell(double x, double y, double w, double h, int state) {
 		super(x, y, w, h, state);
 		// TODO Auto-generated constructor stub
+		nestCell = state == NEST_CELL;
+		foodCell = state == FOOD_CELL;
+	}
+	
+	public void syncState(){
+		if (this.getState() != this.getNState()) {
+			this.setState(this.getNState());
+			int temp = this.getState();
+			nestCell = temp == NEST_CELL;
+			foodCell = temp == FOOD_CELL;
+		}
+	}
+	
+	public List<Ant> getAnts(){
+		return occupyingAnts;
+	}
+	
+	public void setAnts(List<Ant> antList){
+		occupyingAnts = antList;
+	}
+	
+	public int getFood(){
+		return foodAmount;
+	}
+	
+	public boolean isNest(){
+		return nestCell;
+	}
+	
+	public boolean isFood(){
+		return foodCell;
+	}
+	
+	public double getFoodScent(){
+		return foodScent;
+	}
+	
+	public double getHomeScent(){
+		return homeScent;
 	}
 
 }
