f66f5b5e25be96995cac26fecd4c0e7070351c6b
Gordon Huynh <gordon.huynh@gmail.com>
2017-02-09 11:23:37 -0500

    foraging ants

--- src/core/ForagingAntCell.java
+++ src/core/ForagingAntCell.java
@@ -1,6 +1,9 @@
 package core;
 
+import java.util.List;
+
 public class ForagingAntCell extends Cell{
+	private List<Ant> occupyingAnts;
 
 	public ForagingAntCell(double x, double y, double w, double h, int state) {
 		super(x, y, w, h, state);
