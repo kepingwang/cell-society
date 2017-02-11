7576e84987ad6cf4e319ab78be5d99cd10cb6de0
Gordon Huynh <ghh6@duke.edu>
2017-02-11 18:29:25 -0500

    wip wator

--- src/refactor/cell/games/WatorCell.java
+++ src/refactor/cell/games/WatorCell.java
@@ -1,6 +1,7 @@
 package refactor.cell.games;
 
 import java.util.List;
+import java.util.Random;
 
 import javafx.scene.paint.Color;
 import refactor.cell.Cell;
@@ -9,29 +10,44 @@ public class WatorCell extends SimpleCell {
 	private final static int WATER = 0;
 	private final static int FISH = 1;
 	private final static int SHARK = 2;
-	private int energy;
 	
+	private final int fishBirth;
+	private final int sharkBirth;
+	private final int sharkDeath;
+	private int energy;
+	private Random watorRNG = new Random();
 
 	public WatorCell(String cellShapeType, Color[] colors, List<Double> params, int state) {
 		super(cellShapeType, colors, params, state);
-		
+		this.nextState = state;
+		fishBirth = params.get(0).intValue();
+		sharkBirth = params.get(1).intValue();
+		sharkDeath = params.get(2).intValue();
+		energy = 0;
 	}
 
-	@Override
+	@SuppressWarnings("unchecked")
 	public void updateNextState() {
 		// TODO Auto-generated method stub
+		if(this.currState == WATER){
+			return;
+		}
+		List<WatorCell> neighbors = (List<WatorCell>) this.getNeighbors();
+		if(this.currState == this.nextState){
+			if(this.currState == FISH){
+				updateFish();
+			}
+			else if(this.currState == SHARK){
+				updateShark();
+			}
+		}
+	}
+	
+	private void updateFish(){
 		
 	}
-
-	@Override
-	public void syncState() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void syncView(int i, int j, double cw, double ch) {
-		// TODO Auto-generated method stub
+	
+	private void updateShark(){
 		
 	}
 
