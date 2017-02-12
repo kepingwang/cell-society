1285c87d6ad708392dc00e3fe02cba7bc09ae66e
Gordon Huynh <ghh6@duke.edu>
2017-02-11 22:06:45 -0500

    Wator and ForagingAnt done

---
+++ src/refactor/cell/games/ForagingAntCell.java
@@ -0,0 +1,127 @@
+package refactor.cell.games;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import javafx.scene.paint.Color;
+
+public class ForagingAntCell extends SimpleCell{
+	public static final int NEUTRAL_CELL = 0;
+	public static final int NEST_CELL = 1;
+	public static final int FOOD_CELL = 2;
+	private static final int INIT_FOOD_AMOUNT = 50;
+	private static final double DECAY_RATE = .99;
+	private static final int ANT_SPAWN_RATE = 2;
+	
+	private List<Ant> ants;
+	private boolean nestCell;
+	private boolean foodCell;
+	private double foodScent;
+	private double homeScent;
+	private int foodAmount;
+	
+	private ArrayList<ForagingAntCell> neighbors;
+
+	public ForagingAntCell(String cellShapeType, Color[] colors, List<Double> params, int state) {
+		super(cellShapeType, colors, params, state);
+		this.nextState = state;
+		nestCell = state == NEST_CELL;
+		foodCell = state == FOOD_CELL;
+		if(foodCell){
+			foodAmount = INIT_FOOD_AMOUNT;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void updateNextState() {
+		this.decayScent();
+		neighbors = (ArrayList<ForagingAntCell>) this.getNeighbors();
+		if(this.currState == NEST_CELL){
+			this.updateNest();
+			return;
+		}
+		if(this.currState == FOOD_CELL){
+			this.updateFood();
+			return;
+		}
+		moveAnts();
+	}
+	
+	private void updateNest(){
+		moveAnts();
+		for(int i = 0; i < ANT_SPAWN_RATE; i++){
+			ants.add(new Ant(this));
+		}
+	}
+	
+	private void updateFood(){
+		moveAnts();
+		if(this.foodAmount <= 0){
+			this.nextState = NEUTRAL_CELL;
+		}
+	}
+	
+	private void moveAnts(){
+		ArrayList<Ant> antsToRemove = new ArrayList<Ant>();
+		for(Ant a : ants){
+			if(a.forage(neighbors) == 1){
+				antsToRemove.add(a);
+			}
+		}
+		ants.removeAll(antsToRemove);
+	}
+	
+	private void decayScent(){
+		foodScent = foodScent * DECAY_RATE;
+		homeScent = homeScent * DECAY_RATE;
+	}
+
+	public void syncState(){
+		viewSynchronized = (currState == nextState);
+		currState = nextState;
+		nestCell = currState == NEST_CELL;
+		foodCell = currState == FOOD_CELL;
+	}
+	
+	public boolean isFood(){
+		return foodCell;
+	}
+	
+	public boolean isNest(){
+		return nestCell;
+	}
+	
+	public void incrementFood(){
+		foodAmount++;
+	}
+	
+	public void decrementFood(){
+		foodAmount--;
+	}
+	
+	public List<Ant> getAnts(){
+		return ants;
+	}
+	
+	public void setAnts(List<Ant> antsIn){
+		ants = antsIn;
+	}
+	
+	public void setFoodScent(double d){
+		foodScent = d;
+	}
+	
+	public double getFoodScent(){
+		return foodScent;
+	}
+	
+	public void setHomeScent(double d){
+		homeScent = d;
+	}
+	
+	public double getHomeScent(){
+		return homeScent;
+	}
+}
--- src/refactor/cell/games/WatorCell.java
+++ src/refactor/cell/games/WatorCell.java
@@ -1,10 +1,10 @@
 package refactor.cell.games;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import javafx.scene.paint.Color;
-import refactor.cell.Cell;
 
 public class WatorCell extends SimpleCell {
 	private final static int WATER = 0;
@@ -14,41 +14,137 @@ public class WatorCell extends SimpleCell {
 	private final int fishBirth;
 	private final int sharkBirth;
 	private final int sharkDeath;
+	private final int eatEnergy;
 	private int energy;
 	private Random watorRNG = new Random();
+	
+	private ArrayList<WatorCell> openSpaces;
 
+	/**
+	 * Params required:
+	 * fishBirth: positive integer
+	 * sharkBirth: positive integer
+	 * sharkDeath: negative integer
+	 * eatEnergy: positive integer
+	 * @param cellShapeType
+	 * @param colors
+	 * @param params
+	 * @param state
+	 */
 	public WatorCell(String cellShapeType, Color[] colors, List<Double> params, int state) {
 		super(cellShapeType, colors, params, state);
 		this.nextState = state;
 		fishBirth = params.get(0).intValue();
 		sharkBirth = params.get(1).intValue();
 		sharkDeath = params.get(2).intValue();
+		eatEnergy = params.get(3).intValue();
 		energy = 0;
 	}
 
 	@SuppressWarnings("unchecked")
 	public void updateNextState() {
-		// TODO Auto-generated method stub
 		if(this.currState == WATER){
 			return;
 		}
 		List<WatorCell> neighbors = (List<WatorCell>) this.getNeighbors();
+		ArrayList<WatorCell> openNeighbors = new ArrayList<WatorCell>();
+		ArrayList<WatorCell> fishNeighbors = new ArrayList<WatorCell>();
+		for(WatorCell n : neighbors){
+			if(n.nextState == WATER){
+				openNeighbors.add(n);
+			}
+			else if(n.currState == FISH && n.nextState == FISH){
+				fishNeighbors.add(n);
+			}
+		}
+		openSpaces = openNeighbors;
+		
 		if(this.currState == this.nextState){
 			if(this.currState == FISH){
 				updateFish();
 			}
 			else if(this.currState == SHARK){
-				updateShark();
+				updateShark(fishNeighbors);
 			}
 		}
 	}
 	
+	/**
+	 * fish moves if possible
+	 */
 	private void updateFish(){
-		
+		this.energy++;
+		if(openSpaces.size() > 0){
+			move(fishBirth);
+		}
 	}
 	
-	private void updateShark(){
-		
+	/**
+	 * checks if shark can eat a fish
+	 * if fish eaten, shark won't move
+	 * @param fishSpaces
+	 */
+	private void updateShark(List<WatorCell> fishSpaces){
+		this.energy--;
+		if(this.energy <= sharkDeath){
+			this.energy = 0;
+			this.nextState = WATER;
+			return;
+		}
+		if(fishSpaces.size() > 0){
+			WatorCell target = fishSpaces.get(watorRNG.nextInt(fishSpaces.size()));
+			this.eat(target);
+			if(this.energy >= sharkBirth && openSpaces.size() > 0){
+				target = openSpaces.get(watorRNG.nextInt(openSpaces.size()));
+				giveBirth(target);
+			}
+		}
+		else if(openSpaces.size() > 0){
+			move(sharkBirth);
+		}
+	}
+	
+	/**
+	 * assumes at least one WatorCell is empty
+	 * picks a random empty cell
+	 * sets empty cell nextState to this cell
+	 * checks if cell can give birth
+	 * @param birthEnergy
+	 */
+	private void move(int birthEnergy){
+		WatorCell target = openSpaces.get(watorRNG.nextInt(openSpaces.size()));
+		target.nextState = this.currState;
+		target.energy = this.energy;
+		this.nextState = WATER;
+		this.energy = 0;
+		if(target.energy >= birthEnergy){
+			giveBirth(this);
+		}
+	}
+	
+	/**
+	 * gives birth to same type of WatorCell
+	 * resets energies to default
+	 * @param location
+	 */
+	private void giveBirth(WatorCell location){
+		location.nextState = this.currState;
+		location.energy = 0;
+		this.energy = 0;
+	}
+	
+	/**
+	 * this cell is assumed to be a shark
+	 * eats given fish by setting it's current state
+	 * and next state to WATER so that "eaten" fish can't move
+	 * allots energy to what is necessary
+	 * @param fish
+	 */
+	private void eat(WatorCell fish){
+		fish.currState = WATER;
+		fish.nextState = WATER;
+		fish.energy = 0;
+		this.energy += eatEnergy;
 	}
 
 }
