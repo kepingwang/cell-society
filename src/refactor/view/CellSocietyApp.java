1e4f4f4442815669ada3ccfa49fe1500f8518c32
Keping <kw238@duke.edu>
2017-02-12 16:02:23 -0500

    reconfigure classpath

--- src/refactor/view/CellSocietyApp.java
+++ src/refactor/view/CellSocietyApp.java
@@ -10,6 +10,8 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class CellSocietyApp extends Application {
+	public static final String INDEX_PAGE_RESOURCE = "/refactor/view/index.fxml";
+	public static final String CSS_RESOURCE = "/refactor/view/main-style.css";
     public static final String DEFAULT_RESOURCE_PACKAGE = "resources/";
     public static final String LANGUAGE = "English";
     private static ResourceBundle resourceBundle;
@@ -19,8 +21,8 @@ public class CellSocietyApp extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		resourceBundle = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + LANGUAGE);
-        String cssForm = getClass().getResource("main-style.css").toExternalForm();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("index.fxml"));
+        String cssForm = getClass().getResource(CSS_RESOURCE).toExternalForm();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(INDEX_PAGE_RESOURCE));
         loader.setResources(getResourceBundle());
 
         HBox root = new HBox();
