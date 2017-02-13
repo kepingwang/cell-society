fea8163126a1d673dfd79d9d5be53471c410c614
Jacob Weiss <jacob.weiss@duke.edu>
2017-02-12 21:41:25 -0500

    Added object for parser

---
+++ src/refactor/config/GameConfig.java
@@ -0,0 +1,22 @@
+package refactor.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GameConfig {
+	private Map<String, String> configMap = new HashMap<String, String>();
+	private int[][] layout;
+	
+	public GameConfig(Map<String, String> configMap, int[][] layout){
+		this.configMap = configMap;
+		this.layout = layout;
+	}
+	
+	public Map<String, String> getConfigMap(){
+		return configMap;
+	}
+	
+	public int[][] getLayout(){
+		return layout;
+	}
+}
